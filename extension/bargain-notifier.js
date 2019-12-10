9a1431ed9df633e4bcf5a7ae79e54ef08023cc93
Shwuup <e_kong98z@hotmail.com>
2019-12-10 19:15:10 +1100

    refactor extension to use local.storage api

--- extension/bargain-notifier.js
+++ extension/bargain-notifier.js
@@ -1,6 +1,6 @@
-function apiCall() {
-  return fetch("http://localhost:8000/bargain", {
-    body: localStorage.getItem("keywordObjectString"),
+function apiCall(payloadBody) {
+  return fetch("https://notifier-backend.herokuapp.com/bargain", {
+    body: payloadBody,
     credentials: "omit",
     method: "POST",
     mode: "cors",
@@ -10,7 +10,8 @@ function apiCall() {
   });
 }
 
-function handleMessage(request, sender, sendResponse) {
+
+function handleMessage(request) {
   console.log("Message from the content script: " + request.action);
   browser.browserAction.setIcon({
     path: {
@@ -21,42 +22,48 @@ function handleMessage(request, sender, sendResponse) {
 }
 
 function createNotification() {
-  let keywordObject = JSON.parse(localStorage.getItem("keywordObjectString"));
-  if (Object.keys(keywordObject["keywords"]).length > 0) {
-    apiCall()
-      .then(response => response.json())
-      .then(jsonResponse => {
-        if (jsonResponse.areThereNewDeals) {
-          //update the state
-          console.log(jsonResponse);
-          delete jsonResponse.areThereNewDeals;
-          localStorage.setItem(
-            "keywordObjectString",
-            JSON.stringify(jsonResponse)
-          );
+  const keywordPromise = browser.storage.local.get()
+  keywordPromise.then((keywordObject) => {
+    if (Object.keys(keywordObject["keywords"]).length > 0) {
+      let payload = { "keywords": keywordObject["keywords"], "numberOfUnclickedKeywords": keywordObject["numberOfUnclickedKeywords"] };
 
-          browser.browserAction.setIcon({
-            path: {
-              16: "icons/icons8-letter-o-16-green.png",
-              32: "icons/icons8-letter-o-32-green.png"
-            }
-          });
+      apiCall(JSON.stringify(payload))
+        .then(response => response.json())
+        .then(jsonResponse => {
+          if (jsonResponse.areThereNewDeals) {
+            //update the state
+            delete jsonResponse.areThereNewDeals;
+            browser.storage.local.set(jsonResponse).then(onSet, onError);
+            browser.browserAction.setIcon({
+              path: {
+                16: "icons/icons8-letter-o-16-green.png",
+                32: "icons/icons8-letter-o-32-green.png"
+              }
+            });
 
-          browser.notifications.create({
-            type: "basic",
-            iconUrl: browser.extension.getURL("icons/border-48.png"),
-            title: "Bargain alert!",
-            message: "New bargain/s on the front page!"
-          });
-        }
-      });
-  }
+            browser.notifications.create({
+              type: "basic",
+              iconUrl: browser.extension.getURL("icons/icons8-letter-o-48-green.png"),
+              title: "Bargain alert!",
+              message: "New bargain/s on the front page!"
+            });
+          }
+        });
+    }
+  })
 }
 
-localStorage.setItem("seenDeals", "{}");
-localStorage.setItem(
-  "keywordObjectString",
-  '{"keywords":{}, "numberOfUnclickedKeywords": 0}'
-);
+function onError(error) {
+  console.log(error);
+}
+function onGet(item) {
+  console.log(`Sucessfully get: ${item}`);
+}
+function onSet() {
+  console.log("Succesfull set");
+}
+
+
+browser.storage.local.set({ keywords: {}, numberOfUnclickedKeywords: 0, seenDeals: {} }).then(onSet, onError);
 browser.runtime.onMessage.addListener(handleMessage);
-setInterval(createNotification, 120000);
+setInterval(createNotification, 60000);
--- extension/popup/itemsofinterest.js
+++ extension/popup/itemsofinterest.js
@@ -1,94 +1,115 @@
-function handleSubmit() {
-  let keyword = document.getElementById("keyword-input").value;
-  let keywordsObject = JSON.parse(localStorage.getItem("keywordObjectString"));
-  keywordsObject["keywords"][keyword] = {
-    offers: [],
-    isOnFrontPage: false,
-    hasUserClicked: false,
-    lastSeenDealIndex: -1
-  };
-
-  localStorage.setItem("keywordObjectString", JSON.stringify(keywordsObject));
+function onGet(item) {
+    console.log(`Sucessfully get: ${JSON.stringify(item)}`);
+    return item;
 }
+function onSet() {
+    console.log("Successfull set");
+}
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+function storeItem(item) {
+    return browser.storage.local.set(item)
+}
+function getItem(item) {
+    return browser.storage.local.get(item)
+}
+function handleSubmit() {
+    const keyword = document.getElementById("keyword-input").value;
+    const keywords = getItem("keywords")
+    keywords.then((keywordObject) => {
+        keywordObject["keywords"][keyword] = {
+            offers: [],
+            isOnFrontPage: false,
+            hasUserClicked: false,
+            lastSeenDealIndex: -1
+        };
+        return keywordObject
+    }).then((keywordObject) => storeItem(keywordObject).then(onSet, onError));
 
+}
 function handleAllBargainClicks() {
-  let keywordsJson = JSON.parse(localStorage.getItem("keywordObjectString"));
-  let keywordArray = Object.keys(keywordsJson.keywords);
-  keywordArray.forEach(keyword => {
-    let keywordInfo = keywordsJson["keywords"][keyword];
-    if (keywordInfo["isOnFrontPage"] && !keywordInfo["hasUserClicked"]) {
-      handleBargainClick(keywordsJson, keyword);
-    }
-  });
+    getItem().then(onGet, onError).then((keywordsJson) => {
+        keywordArray = Object.keys(keywordsJson.keywords);
+        keywordArray.forEach(keyword => {
+            let keywordInfo = keywordsJson["keywords"][keyword];
+            if (keywordInfo["isOnFrontPage"] && !keywordInfo["hasUserClicked"]) {
+                handleBargainClick(keywordsJson, keyword);
+            }
+        });
+    })
 }
 
 function handleBargainClick(keywordsJson, keyword) {
-  let keywordInfo = keywordsJson["keywords"][keyword];
-  let seenDeals = JSON.parse(localStorage.getItem("seenDeals"));
-  let listOfOffers = keywordInfo["offers"];
-  for (
-    let i = keywordInfo["lastSeenDealIndex"] + 1;
-    i < listOfOffers.length;
-    ++i
-  ) {
-    browser.tabs.create({
-      url: listOfOffers[i][0]
-    });
-    if (!seenDeals[keyword]) {
-      seenDeals[keyword] = [listOfOffers[i]];
-    } else {
-      seenDeals[keyword].push(listOfOffers[i]);
-    }
-    localStorage.setItem("seenDeals", JSON.stringify(seenDeals));
-  }
+    let keywordInfo = keywordsJson["keywords"][keyword];
+    const seenDeals = keywordsJson["seenDeals"];
+    let listOfOffers = keywordInfo["offers"];
 
-  keywordInfo["lastSeenDealIndex"] = listOfOffers.length - 1;
-  keywordInfo["hasUserClicked"] = true;
-  if (keywordsJson["numberOfUnclickedKeywords"] > 0) {
-    keywordsJson["numberOfUnclickedKeywords"] -= 1;
-    if (keywordsJson["numberOfUnclickedKeywords"] === 0) {
-      browser.runtime.sendMessage({
-        action: "Change icon color"
-      });
+    for (
+        let i = keywordInfo["lastSeenDealIndex"] + 1;
+        i < listOfOffers.length;
+        ++i
+    ) {
+        browser.tabs.create({
+            url: listOfOffers[i][0]
+        });
+        if (!seenDeals[keyword]) {
+            seenDeals[keyword] = [listOfOffers[i]];
+        } else {
+            seenDeals[keyword].push(listOfOffers[i]);
+        }
     }
-  }
 
-  //update state
-  localStorage.setItem("keywordObjectString", JSON.stringify(keywordsJson));
+    keywordInfo["lastSeenDealIndex"] = listOfOffers.length - 1;
+    keywordInfo["hasUserClicked"] = true;
+
+    if (keywordsJson["numberOfUnclickedKeywords"] > 0) {
+        keywordsJson["numberOfUnclickedKeywords"] -= 1;
+        if (keywordsJson["numberOfUnclickedKeywords"] === 0) {
+            browser.runtime.sendMessage({
+                action: "Change icon color"
+            });
+        }
+    }
+
+    //update state
+    storeItem(keywordsJson).then(onSet, onError);
 }
 
-let keywordsObject = JSON.parse(localStorage.getItem("keywordObjectString"));
-if (Object.keys(keywordsObject).length > 0) {
-  let keywordArray = Object.keys(keywordsObject.keywords);
-  //for each keyword, make a clikable/non-clickable div with a discard button and insert into grid
-  keywordArray.forEach(keyword => {
-    let keywordInfo = keywordsObject["keywords"][keyword];
-    let div = document.createElement("div");
-    if (keywordInfo["isOnFrontPage"] && !keywordInfo["hasUserClicked"]) {
-      div.className = "keyword-frontpage";
-      div.addEventListener("click", () =>
-        handleBargainClick(keywordsObject, keyword)
-      );
-    } else {
-      div.className = "keyword";
+
+const keywords = getItem().then(onGet, onError);
+keywords.then((obj) => {
+    let keywordsObject = obj["keywords"]
+    let keywordArray = Object.keys(keywordsObject);
+    if (keywordArray.length > 0) {
+        //for each keyword, make a clickable/non-clickable div with a discard button and insert into grid
+        keywordArray.forEach(keyword => {
+            let keywordInfo = keywordsObject[keyword];
+            let div = document.createElement("div");
+            if (keywordInfo["isOnFrontPage"] && !keywordInfo["hasUserClicked"]) {
+                div.className = "keyword-frontpage";
+                div.addEventListener("click", () =>
+                    handleBargainClick(obj, keyword)
+                );
+            } else {
+                div.className = "keyword";
+            }
+            let text = document.createTextNode(keyword);
+            div.appendChild(text);
+            let closeButton = document.createElement("button");
+            closeButton.append(document.createTextNode("X"));
+            closeButton.className = "close-button";
+            closeButton.onclick = () => {
+                delete keywordsObject[keyword];
+                storeItem(obj).then(onSet, onError);
+                div.style.visibility = "hidden";
+            };
+            div.appendChild(closeButton);
+            document.getElementById("wrapper").appendChild(div);
+        });
     }
-    let text = document.createTextNode(keyword);
-    div.appendChild(text);
-    let closeButton = document.createElement("button");
-    closeButton.append(document.createTextNode("X"));
-    closeButton.className = "close-button";
-    closeButton.onclick = () => {
-      delete keywordsObject["keywords"][keyword];
-      localStorage.setItem(
-        "keywordObjectString",
-        JSON.stringify(keywordsObject)
-      );
-      div.style.visibility = "hidden";
-    };
-    div.appendChild(closeButton);
-    document.getElementById("wrapper").appendChild(div);
-  });
-}
+})
+
 document.getElementById("keyword-input").focus();
 document.addEventListener("submit", handleSubmit);
 document.getElementById("open-all").onclick = handleAllBargainClicks;
